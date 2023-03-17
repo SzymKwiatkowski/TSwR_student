01e916cd61ea4d63d0bed6db45c042e683da8823
Szymon Kwiatkowski <szymon.z.kwiatkowski@gmail.com>
2023-03-17 14:06:51 +0100

    Fix to feedback controller

--- controllers/feedback_linearization_controller.py
+++ controllers/feedback_linearization_controller.py
@@ -4,19 +4,29 @@ from .controller import Controller
 
 
 class FeedbackLinearizationController(Controller):
-    def __init__(self, Tp):
-        self.model = ManipulatorModel(Tp)
+    def __init__(self, Tp, m3=0.0, r3=0.01):
+        self.Tp = Tp
+        self.model = ManipulatorModel(Tp, m3=m3, r3=r3)
+        self.Kd = 100.0
+        self.Kp = 100.0
 
     def calculate_control(self, x, q_r, q_r_dot, q_r_ddot):
         """
         Please implement the feedback linearization using self.model (which you have to implement also),
         robot state x and desired control v.
         """
-
+        # q, q_dot = np.hsplit(x[np.newaxis,:], 2)
+        # q = np.transpose(np.array(q))
+        # q_dot = np.transpose(np.array(q_dot))
+        q = np.transpose(np.array([x[:2]]))
+        q_dot = np.transpose(np.array([x[2:]]))
         M = self.model.M(x)
         C = self.model.C(x)
-        q_r_ddot = np.array([q_r_ddot])
-        q_r_dot = np.array([q_r_dot])
+        q_r_ddot = np.transpose(np.array([q_r_ddot]))
+        q_r_dot = np.transpose(np.array([q_r_dot]))
+        q_r = np.transpose(np.array([q_r]))
 
-        tau = M @ np.transpose(q_r_ddot) + C @ np.transpose(q_r_dot)
+        v = q_r_ddot - self.Kd * ( q_dot - q_r_dot ) - self.Kp * (q - q_r)
+
+        tau = M @ v + C @ q_dot
         return tau
--- flc.py
+++ flc.py
@@ -8,10 +8,11 @@ from trajectory_generators.constant_torque import ConstantTorque
 from trajectory_generators.sinusonidal import Sinusoidal
 from trajectory_generators.poly3 import Poly3
 from utils.simulation import simulate
+from models.manipulator_model import ManipulatorModel
 
 Tp = 0.01
 start = 0
-end = 3
+end = 10
 
 """
 Switch to FeedbackLinearizationController as soon as you implement it
@@ -24,8 +25,8 @@ Here you have some trajectory generators. You can use them to check your impleme
 At the end implement Point2point trajectory generator to move your manipulator to some desired state.
 """
 # traj_gen = ConstantTorque(np.array([0., 1.0])[:, np.newaxis])
-# traj_gen = Sinusoidal(np.array([0., 1.]), np.array([2., 2.]), np.array([0., 0.]))
-traj_gen = Poly3(np.array([0., 0.]), np.array([math.pi/4, math.pi/6]), end)
+traj_gen = Sinusoidal(np.array([0., 1.]), np.array([2., 2.]), np.array([0., 0.]))
+# traj_gen = Poly3(np.array([0., 0.]), np.array([math.pi/4, math.pi/6]), end)
 
 
 Q, Q_d, u, T = simulate("PYBULLET", traj_gen, controller, Tp, end)
@@ -36,12 +37,15 @@ You can add here some plots of the state 'Q' (consists of q and q_dot), controls
 with respect to time 'T' to analyze what is going on in the system
 """
 plt.subplot(221)
-plt.plot(T, Q[:, 0], 'r')
-plt.plot(T, Q_d[:, 0], 'b')
+plt.plot(T, Q[:, 0], 'r', label="q_1")
+plt.plot(T, Q_d[:, 0], 'b', label="qd_1")
+plt.legend()
 plt.subplot(222)
-plt.plot(T, Q[:, 1], 'r')
-plt.plot(T, Q_d[:, 1], 'b')
+plt.plot(T, Q[:, 1], 'r', label="q_2")
+plt.plot(T, Q_d[:, 1], 'b', label="qd_2")
+plt.legend()
 plt.subplot(223)
-plt.plot(T, u[:, 0], 'r')
-plt.plot(T, u[:, 1], 'b')
+plt.plot(T, u[:, 0], 'r', label="u_1")
+plt.plot(T, u[:, 1], 'b', label="u_2")
+plt.legend()
 plt.show()
